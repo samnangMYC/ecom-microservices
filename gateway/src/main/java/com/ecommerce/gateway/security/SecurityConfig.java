133acb116ef9b8ab100cc615107a0b1c6ccd3b84
samnang <samnangproducer@gmail.com>
2025-10-24 18:37:27 +0700

    add docker with env

--- gateway/src/main/java/com/ecommerce/gateway/security/SecurityConfig.java
+++ gateway/src/main/java/com/ecommerce/gateway/security/SecurityConfig.java
@@ -1,14 +1,17 @@
 package com.ecommerce.gateway.security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.config.environment.Environment;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import reactor.core.publisher.Flux;
@@ -53,4 +56,6 @@ public class SecurityConfig {
         });
         return jwtAuthenticationConverter;
     }
+
+
 }
